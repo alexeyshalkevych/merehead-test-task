5f02e4865d6efe727b68bc3509de18e4c30ddb97
Alexey <valnistuy@gmail.com>
2020-11-15 18:28:53 +0200

    :refactor: change BrowserRouter to HashRouter and added basename attribute in index.js for deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './components/App/App';
 import store from './store';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter basename="/">
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root'),
 );
