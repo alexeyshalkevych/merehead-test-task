8c9b77263bd8d55078a18e3dfd6d4fb681bfecf8
Alexey <valnistuy@gmail.com>
2020-11-15 18:48:37 +0200

    :refactor: remove gh-pages and added surge package for deploy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import store from './store';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter basename="/">
+    <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
